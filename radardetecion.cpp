a23f6de85436a42dbb8ca84f38885267151835aa
Endlessfancy <60722519+Endlessfancy@users.noreply.github.com>
2023-06-14 11:29:27 -0400

    Update radardetecion.cpp

--- radardetecion.cpp
+++ radardetecion.cpp
@@ -85,9 +85,9 @@ void Complex_matrixMUL(MKL_Complex16 *M_res, MKL_Complex16 *M_A, MKL_Complex16 *
     }
 }
 
-  double Complex_mol(MKL_Complex16 *a){
-    return sqrt(a->real*a->real+a->imag*a->imag);
-  }
+double Complex_mol(MKL_Complex16 *a){
+  return sqrt(a->real*a->real+a->imag*a->imag);
+}
 
 int FindAbsMax(MKL_Complex16 *ptr, int size){
     int maxidx = 0;
@@ -103,7 +103,31 @@ int FindAbsMax(MKL_Complex16 *ptr, int size){
     return maxidx;
 }
 
+void Find2dAbsMax(MKL_Complex16 **ptr, int rowsize, int colsize, int *maxidx){
+    maxidx[0] = 0;
+    maxidx[1] = 0;
+    double maxval = 0;
+    double absval;
+    for (int i = 0; i < rowsize; i++){
+      for (int j = 0; j < colsize ; j++){
+        absval = Complex_mol((&ptr[i][j]));
+        if(absval > maxval){
+            maxval = absval;
+            maxidx[0] = i;
+            maxidx[1] = j;
+        }
+      }
+    }
+    return ;
+}
+
 void Matrix_Transpose(MKL_Complex16 *M[], MKL_Complex16 *M_res[], int sizeRow, int sizeCol){
+    for (int i = 0; i < 128; i++){
+        for (int j =0; j < 128; j++){
+            M_res[i][j] = M[i][j];
+            // *(M_res + i*sizeCol + j) = *(M + j* sizeRow + i);
+        }
+    }
     for (int i = 0; i < sizeRow; i++){
         for (int j =0; j < sizeCol; j++){
             M_res[i][j] = M[j][i];
@@ -112,8 +136,8 @@ void Matrix_Transpose(MKL_Complex16 *M[], MKL_Complex16 *M_res[], int sizeRow, i
     }
 }
 
-void printComplex(MKL_Complex16 *Data, int size){
-    printf("Print!\n");
+void printComplex(MKL_Complex16 *Data, int size, char string[]){
+    printf("Print %s!\n", string);
     for (int i = 0; i < size; i++){
       printf("data %d = %lf+i*%lf\n", i, (Data+i)->real,  (Data+i)->imag);
     }
@@ -269,15 +293,6 @@ int main() {
     // run the kernel 
     //# initialize data on host
     //# Explicit USM allocation using malloc_device
-    // buffer
-    // vector<short> DataHost_Frm0_read(size);
-    // vector<short> DataHost_Frm_read (size);
-    // vector<MKL_Complex16> DataHost_Frm0_reshape(size);
-    // vector<double> Dis(89);
-    // short(* DataHost_Frm0_read) = new short[size];
-    // short(* DataHost_Frm_read) = new short[size];
-    // MKL_Complex16(* DataHost_Frm0_reshape) = new MKL_Complex16[size];
-    // MKL_Complex16(* DataHost_Frm_reshape) = new MKL_Complex16[size];
     // implicit USM
     short *DataShared_Frm0_read = malloc_shared<short>(ChirpSize*SampleSize*RxSize*2, q);
     short *DataShared_Frm_read = malloc_shared<short>(ChirpSize*SampleSize*RxSize*2, q);
@@ -296,20 +311,16 @@ int main() {
     for(int i = 0; i < SampleSize_extend; i++){
       DataShared_2dFFT_tp[i] = malloc_shared<MKL_Complex16>(ChirpSize_extend, q);
     }
-    // MKL_Complex16 *DataShared_2dFFT_tp = malloc_shared<MKL_Complex16>(extendSize, q);
-    // MKL_Complex16 *DataShared_chunkFFT = malloc_shared<MKL_Complex16>(128, q);
-    // int *FFT_indexlist = malloc_shared<int>(extendSize, q);
-    double *Dis = malloc_shared<double>(89, q);
-    double *Spd = malloc_shared<double>(89, q);
-    double *Agl = malloc_shared<double>(89, q);
-    double *SDis = malloc_shared<double>(89, q);
-    int *maxDisIdx = malloc_shared<int>(89, q);
-    int *maxSpdIdx = malloc_shared<int>(89, q);
-    int *maxSDisIdx = malloc_shared<int>(89, q);
-    int *maxAglIdx = malloc_shared<int>(89, q);
+    double *Dis = malloc_shared<double>(91, q);
+    double *Spd = malloc_shared<double>(91, q);
+    double *Agl = malloc_shared<double>(91, q);
+    double *SDis = malloc_shared<double>(91, q);
+    int *maxDisIdx = malloc_shared<int>(91, q);
+    int *maxSpdIdx = malloc_shared<int>(91, q);
+    int *maxSDisIdx = malloc_shared<int>(91, q);
+    int *maxAglIdx = malloc_shared<int>(91, q);
     
     // initial the matrix
-    
     int agl_sampleNum = 180/0.1+1;
     double theta;
     MKL_Complex16 *Agl_matrix = malloc_shared<MKL_Complex16>(4*agl_sampleNum, q);
@@ -326,22 +337,13 @@ int main() {
         }
     }
 
-
-
-    // get the data
+    // get the data frame0
     fread(DataShared_Frm0_read, sizeof(short), ChirpSize*SampleSize*RxSize*2, infile);
     ReshapeComplex(DataShared_Frm0_read, DataShared_Frm0_reshape, readsize);
 
-    // buffer
-//     buffer<short, 1> buf_Frm0_read(DataHost_Frm0_read, size);
-//     buffer<short, 1> buf_Frm_read(DataHost_Frm_read, size);
-    //buffer<short, 1> buf_Frm0_read(DataHost_Frm0_read, size);
-
-    // q.memcpy(DataDevc_Frm0, DataDecv_Frm0, sizeof(MKL_Complex16) * size).wait();
-
     int frm = 0;
     while(1){
-        if(frm == 60 )break;
+        // if(frm == 89 )break;
         frm++;
         // q.memcpy(DataDevc_Frm, DataDecv_Frm,, sizeof(MKL_Complex16) * size).wait();
         
@@ -355,6 +357,7 @@ int main() {
 
         
         // ******************************************************** distance *****************************************************************//            
+        //-------device------//
         auto e = q.submit([&](handler& h) {
             // Preprocess: minus the frm0 data
             h.parallel_for(range<1>(extendSize), [=](id<1> i) { 
@@ -368,9 +371,9 @@ int main() {
             });
         });
 
-
         oneAPI_FFT(DataShared_Rx_extended, DataShared_FFT, extendSize);
 
+        //-------host------//
         int maxidx = FindAbsMax(DataShared_FFT, floor(0.4*extendSize));
         // int extendSize = getextendsize(ChirpSize*SampleSize);
         double Fs_extend = Fs * extendSize/(ChirpSize*SampleSize);
@@ -381,7 +384,7 @@ int main() {
         maxDisIdx[frm] = maxDisidx;
         // ******************************************************** distance *****************************************************************//
         // ******************************************************** speed *****************************************************************//
-
+        //-------device------//
         for(int chp = 0; chp < ChirpSize_extend; chp++){
             // do the 1dfft for the data in rx0
             MKL_Complex16 *chp_ptr = DataShared_Rx + chp * SampleSize;
@@ -399,10 +402,14 @@ int main() {
             // MKL_Complex16 *fft1dres_ptr = DataShared_2dFFT+chp * SampleSize_extend;
             // do fft for the data in Data_frm_rx0, the result is stored in Data_2dfft
             // a new version of FFTextend_OMP but divide N here:
-            oneAPI_FFT_inplace(DataShared_2dFFT[chp],  SampleSize_extend);
+            // oneAPI_FFT_inplace(DataShared_2dFFT[chp],  SampleSize_extend);
 
         }
-
+        // printComplex(DataShared_2dFFT[0], 10, "in");
+        for(int chp = 0; chp < ChirpSize_extend; chp++){
+            oneAPI_FFT_inplace(DataShared_2dFFT[chp],  SampleSize_extend);
+        }
+        // printComplex(DataShared_2dFFT[0], 128, "fft row");
         // 2dfft: do the fft again for each column for the 1dfft result
         // ||||
         // ||||
@@ -410,106 +417,88 @@ int main() {
         int chp_mid = ChirpSize_extend/2 ;
         int smp_mid = SampleSize_extend/2;
         // transpose the matrix
-        Matrix_Transpose(DataShared_2dFFT, DataShared_2dFFT_tp, ChirpSize_extend, SampleSize_extend);
+        Matrix_Transpose(DataShared_2dFFT, DataShared_2dFFT_tp, ChirpSize_extend, SampleSize);
+       
         for(int smp = 0; smp < SampleSize_extend; smp++){
             // do the 1dfft for the data in rx0
             // MKL_Complex16 *smp_ptr = DataShared_2dFFT_tp + smp * ChirpSize_extend;
-    
             oneAPI_FFT_inplace(DataShared_2dFFT_tp[smp], ChirpSize_extend);
+        }
+        for(int smp = 0; smp < SampleSize_extend; smp++){
+            // do the fftshift
             // fft shift, exchange the data before mid and after mid
             FFTshift(DataShared_2dFFT_tp[smp], ChirpSize_extend);
             // set the mid to 0
             DataShared_2dFFT_tp[smp][chp_mid] = GetComplex(0, 0);
             // *(smp_ptr + chp_mid) = GetComplex(0, 0);
         }
-        // 2dFFT
-        // for (int i  = 0 ; i< 127; i++){
-        //   for(int j = 0; j< 127; j++){
-        //     DataShared_2dFFT[i][j] = GetComplex(0, 0);
-        //   }
-        // }
-
-        // q.submit([&](handler& h) {
-        // // Preprocess: minus the frm0 data
-        // h.parallel_for(range<1>(extendSize), [=](id<1> i) {
-        //   int row = i / SampleSize_extend;
-        //   int col = i % SampleSize_extend;
-        //   int idx = row * SampleSize + col ;
-        //   if(col < SampleSize ){
-        //     // DataShared_2dFFT[row][col] = DataShared_Rx[idx];
-        //     DataShared_2dFFT[row][col].real = row ;
-        //     DataShared_2dFFT[row][col].imag = col ;
-        //   }
-        //   else{
-        //     DataShared_2dFFT[row][col] = GetComplex(0, 0);
-        //   }
-        // });
-          
-        //     });
-            // MKL_Complex16 *fft1dres_ptr = DataShared_2dFFT+chp * SampleSize_extend;
-            // // do fft for the data in Data_frm_rx0, the result is stored in Data_2dfft
-            // // a new version of FFTextend_OMP but divide N here:
-            // oneAPI_FFT(DataShared_chunkFFT, fft1dres_ptr, SampleSize_extend);
-        // for (int i = 0; i < 1; i++)
-        // for (int j = 0; j < 128; j++)
-        //     std::cout << i << ' '
-        //               << DataShared_2dFFT[i][j].real << "+j" << DataShared_2dFFT[i][j].imag << "  "
-        //               << DataShared_2dFFT[i+1][j].real << "+j" << DataShared_2dFFT[i+1][j].imag << "  "
-        //               // << DataShared_2dFFT[i][100].real << "+j" << DataShared_2dFFT[i][100].imag
-        //               << "\n ";
-        int max2dfft =FindAbsMax(DataShared_2dFFT_tp[0],  ChirpSize * SampleSize);
-        // TODO: make sure the sequence of this is totally correct
-        // col
-        int maxSDisidx = FindAbsMax(DataShared_2dFFT_tp[0], ChirpSize * SampleSize) / ChirpSize;
-        // row
-        int maxSpdidx = FindAbsMax(DataShared_2dFFT_tp[0], ChirpSize * SampleSize) % ChirpSize;
-        // int maxSpdidx = FindAbsMax(Data_2dfft, ChirpSize * SampleSize) % ChirpSize;
-        // int maxSDisidx = FindAbsMax(Data_2dfft, ChirpSize * SampleSize) / ChirpSize;
+       
+        //-------host------//
+        int maxSDisidx = 0;
+        int maxSpdidx = 0;
+        double maxval = 0;
+        double absval= 0;
+        for (int i = 0; i < 0.4* SampleSize_extend; i++){
+          for (int j = 0; j <  ChirpSize_extend ; j++){
+            absval = Complex_mol((&DataShared_2dFFT_tp[i][j]));
+            if(absval > maxval){
+              maxval = absval;
+              maxSDisidx = i;
+              maxSpdidx = j;
+          }
+          }
+        }  
+        // int maxSDisidx = max2didx[0];
+        // int maxSpdidx = max2didx[1];
         double maxSpd = ((maxSpdidx)*fr/ChirpSize - fr/2)*lamda/2;
-        double maxSDis = ((maxSDisidx)*Fs/SampleSize )*cspd/(2*mu);
+        double maxSDis = ((maxSDisidx)*Fs /(SampleSize_extend) )*cspd/(2*mu);
         // the whole formula  maxDis = c*(((double)maxDisidx*Fs/(ChirpSize*SampleSize)))/(2*mu);
         Spd[frm] = maxSpd ;
         maxSpdIdx[frm] = maxSpdidx;
         SDis[frm] = maxSDis ;
         maxSDisIdx[frm] = maxSDisidx;
+   
 
         // ******************************************************** speed *****************************************************************//
         // ******************************************************** Angle *****************************************************************//
-        // int maxAglidx = maxidx;
-        // MKL_Complex16 Agl_weight[4];
-        // // fft result / length
-        // // TODO: you can directly add the division of N in the FFT_extend function.
-        // Agl_weight[0] = GetComplex(DataShared_FFT[maxAglidx].real/extendSize, DataShared_FFT[maxAglidx].imag/extendSize);
+        //-------host------//
+        int maxAglidx = maxidx;
+        MKL_Complex16 Agl_weight[4];
+        // fft result / length
+        // TODO: you can directly add the division of N in the FFT_extend function.
+        Agl_weight[0] = GetComplex(DataShared_FFT[maxAglidx].real/extendSize, DataShared_FFT[maxAglidx].imag/extendSize);
 
-        // for (int j= 1; j < RxSize; j++){
-        //     auto e = q.submit([&](handler& h) {
-        //     // Preprocess: minus the frm0 data
-        //     h.parallel_for(range<1>(extendSize), [=](id<1> i) { 
-        //         if(i < SampleSize * ChirpSize ){
-        //           DataShared_Rx[i] = Complex_SUB(DataShared_Frm_reshape[i+j*ChirpSize*SampleSize], DataShared_Frm0_reshape[i]);
-        //           DataShared_Rx_extended[i] = DataShared_Rx[i];
-        //         }
-        //         else{
-        //           DataShared_Rx_extended[i] = GetComplex(0, 0);
-        //         }
+        //-------device------//
+        for (int j= 1; j < RxSize; j++){
+            auto e = q.submit([&](handler& h) {
+            // Preprocess: minus the frm0 data
+            h.parallel_for(range<1>(extendSize), [=](id<1> i) { 
+                if(i < SampleSize * ChirpSize ){
+                  DataShared_Rx[i] = Complex_SUB(DataShared_Frm_reshape[i+j*ChirpSize*SampleSize], DataShared_Frm0_reshape[i]);
+                  DataShared_Rx_extended[i] = DataShared_Rx[i];
+                }
+                else{
+                  DataShared_Rx_extended[i] = GetComplex(0, 0);
+                }
                 
-        //     });
-        // });
-        //     // do FFT in for rx0, rx1, rx2
-        //     oneAPI_FFT(DataShared_Rx_extended, DataShared_FFT, extendSize);
-        //     // get the max data with the maxidx we get in the DisDetection code.
-        //     Agl_weight[j] = Complex_CDiv(*(DataShared_FFT + maxAglidx), extendSize);
-        // }
+            });
+        });
+            // do FFT in for rx0, rx1, rx2
+            oneAPI_FFT(DataShared_Rx_extended, DataShared_FFT, extendSize);
+            // get the max data with the maxidx we get in the DisDetection code.
+            Agl_weight[j] = Complex_CDiv(*(DataShared_FFT + maxAglidx), extendSize);
+        }
 
-        // //--------------- MMM ----------------- 
-        // Complex_matrixMUL(Agl_mulRes, Agl_weight, Agl_matrix, 1, 4, agl_sampleNum);
+        //--------------- MMM ----------------- 
+        Complex_matrixMUL(Agl_mulRes, Agl_weight, Agl_matrix, 1, 4, agl_sampleNum);
         
-        // //--------------- find Max -----------------
-        // maxAglidx = FindAbsMax(Agl_mulRes, agl_sampleNum);
-        // double maxAgl = (maxAglidx-900.0)/10.0;
-        // double maxPhi = (maxAglidx-900.0)/10.0/180*pi;
-        // Agl[frm] = maxAgl ;
-        // maxAglIdx[frm] = maxAglidx;
+        //-------host------//
+        //--------------- find Max -----------------
+        maxAglidx = FindAbsMax(Agl_mulRes, agl_sampleNum);
+        double maxAgl = (maxAglidx-900.0)/10.0;
+        double maxPhi = (maxAglidx-900.0)/10.0/180*pi;
+        Agl[frm] = maxAgl ;
+        maxAglIdx[frm] = maxAglidx;
 
         // ******************************************************** Angle *****************************************************************//
     }
@@ -517,7 +506,7 @@ int main() {
     //# print output
     // printComplex(DataShared_Frm_reshape, 10);
     // std::cout << frm << "\n"; 
-    for (int i = 0; i < frm; i++)
+    for (int i =1; i < frm + 1; i++)
      std::cout << i <<" maxDisidx "<< maxDisIdx[i] << " Dis " << Dis[i]
                     <<" maxAglidx "<< maxAglIdx[i] << " Agl " << Agl[i]
                     <<" maxSpdidx "<< maxSpdIdx[i] << " Spd " << Spd[i]
@@ -573,49 +562,3 @@ int main() {
   return 0;
 }
 
-// int main(){
-// // sycl::queue Q(sycl::default_selector{});
-// // std::cout << "Running on: "
-// //             << Q.get_device().get_info<sycl::info::device::name>() << "\n";
-// // float _Complex c2c_data[32];
-// MKL_Complex16 c2c_input[32];
-// MKL_Complex16 c2c_output[32];
-
-// MKL_Complex16 c2c_temp[32];
-// // float r2c_data[34];
-// DFTI_DESCRIPTOR_HANDLE my_desc1_handle = NULL;
-// DFTI_DESCRIPTOR_HANDLE my_desc2_handle = NULL;
-// MKL_LONG status;
-
-// /* ...put values into c2c_data[i] 0<=i<=31 */
-// /* ...put values into r2c_data[i] 0<=i<=31 */
-
-// for (int i=0; i< 32; i++){
-//     c2c_input[i].real = (double)i ;
-//     c2c_input[i].imag = (double)i ;
-//     c2c_output[i].real = (double)0 ;
-//     c2c_output[i].imag = (double)0 ;
-// }
-// std::cout<<"Printing input result "<<"\n";
-// for (int i=0 ; i< 32; i++){
-//     std::cout<<i<<" real "<<c2c_input[i].real<<" imag "<<c2c_input[i].imag<<"\n";
-// }
-// status = DftiCreateDescriptor(&my_desc1_handle, DFTI_DOUBLE,
-//                               DFTI_COMPLEX, 1, 32);
-// status = DftiSetValue(my_desc1_handle, DFTI_PLACEMENT, DFTI_NOT_INPLACE);
-// status = DftiCommitDescriptor(my_desc1_handle);
-// status = DftiComputeForward(my_desc1_handle, c2c_input, c2c_temp);
-// status = DftiComputeBackward(my_desc1_handle, c2c_temp, c2c_output);
-// status = DftiFreeDescriptor(&my_desc1_handle);
-
-// std::cout<<"Printing output result "<<"\n";
-// for (int i=0; i< 32; i++){
-//     std::cout<<i<<" real "<<c2c_temp[i].real<<" imag "<<c2c_temp[i].imag<<"\n";
-// }
-
-// std::cout<<"Printing output result "<<"\n";
-// for (int i=0; i< 32; i++){
-//     std::cout<<i<<" real "<<c2c_output[i].real<<" imag "<<c2c_output[i].imag<<"\n";
-// }
-//     return 0 ;
-// }
